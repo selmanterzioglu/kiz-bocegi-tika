d7cc40f578492e5ad62a231d4474911dbcf864d5
selmantercioglu <selmantercioglu@gmail.com>
2022-02-07 22:24:30 +0300

    added modified files of prototip1

--- prototip1/main_window_constructor.py
+++ prototip1/main_window_constructor.py
@@ -1,41 +1,77 @@
-from PyQt5.QtWidgets import *
 import libs
 
+import logging
+from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtGui import QImage, QPixmap
+from structure_ui import Structure_UI
+from structure_ui_camera import Structure_Ui_Camera
+from structure_camera import Camera_Object, CAMERA_FLAGS
 import sys
 import cv2
-from cv2 import imread
-import numpy as np
 from qt_tools import numpy_To_QT_Type_Converter
 
-class MyWindow(QMainWindow):
-    def __init__(self):
-        super(MyWindow, self).__init__()
-        uic.loadUi('main_window.ui', self)
-        self.show()
+class Ui_Camera_API_Main(Structure_UI):
+    
+    logger_level = logging.INFO
 
-        self.frame_1.setScene(QGraphicsScene())
+    def __init__(self, *args, obj=None, logger_level= logging.INFO, **kwargs):
+        super(Ui_Camera_API_Main, self).__init__(*args, **kwargs)
 
-    def clicked_function(self):
+        self.logger_level = logger_level
+        self.init()
+    
+
+    def init(self):
+        self.configure_Other_Settings()
+    
+    def connect_camera_button_triggered(self):
+        print("test")
+
+    
         
-        self.label1.setText("said")
-        img = imread("test.png", 0)
-        # img = QPixmap("test.png")
-        img = numpy_To_QT_Type_Converter(img)
-        self.frame_1.scene().addPixmap(img)
 
+class Ui_Camera_API_Developer(Structure_Ui_Camera):
+    logger_level = logging.INFO
 
-# class Graphics_View(QGraphicsView): 
-#     def __init__(self, *args, parent=None, obj=None, **kwargs):
-#         super(Graphics_View, self).__init__(*args, **kwargs)
-#         self.current_Frame = None
-#         self.setScene(QGraphicsScene(parent=parent))
+    def __init__(self, *args, onj = None, logger_level = logging.INFO, **kwargs):
+        super(Ui_Camera_API_Developer, self).__init__(*args, **"kwargs")
+
+        self.logger_level = logger_level
+
+        self.init()
+    
+    def init(self):
+        self.configure_Other_Settings()
+
+    def configure_Button_Connections(self):
+        self.connect_camera_button.clicked.connect(
+            lambda: self.connect_to_Camera(
+                camera_flag=CAMERA_FLAGS.CV2
+                
+            )
+        )
+
+    def configure_Other_Settings(self):
         
-#     def set_Scene(self, parent=None, scene=None):
-#         self.setScene(QGraphicsScene(parent=parent) if scene is None else scene)
+        self.init_qt_graphicsView_Scene(
+            self.frame_1,
+        )
+        self.init_qt_graphicsView_Scene(
+            self.frame_2,
+        )
+        self.init_qt_graphicsView_Scene(
+            self.frame_3,
+        )
+        self.init_qt_graphicsView_Scene(
+            self.frame_4,
+        )
+
+    def closeEvent(self, *args, **kwargs):
+        super(Ui_Camera_API_Developer, self).closeEvent(*args, **kwargs)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MyWindow()
+    window = Ui_Camera_API_Main()
     sys.exit(app.exec_())
