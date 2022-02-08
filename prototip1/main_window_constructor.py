52776860f2fb2b80905ad5ce4b11c2046cc660b2
selmantercioglu <selmantercioglu@gmail.com>
2022-02-08 16:13:31 +0300

    added files

--- prototip1/main_window_constructor.py
+++ prototip1/main_window_constructor.py
@@ -9,7 +9,7 @@ from structure_ui_camera import Structure_Ui_Camera
 from structure_camera import Camera_Object, CAMERA_FLAGS
 import sys
 import cv2
-from qt_tools import numpy_To_QT_Type_Converter
+# from qt_tools import numpy_To_QT_Type_Converter
 
 class Ui_Camera_API_Main(Structure_UI):
     
@@ -25,33 +25,29 @@ class Ui_Camera_API_Main(Structure_UI):
     def init(self):
         self.configure_Other_Settings()
     
-    def connect_camera_button_triggered(self):
-        print("test")
-
+    # def button_Connections(self):
+    #     # clicked_button = self.sender()
+    #     # if clicked_button.objectName() == "connect_camera_button":
+    #     #     self.test()
+    #     self.test()
     
+    # def test(self):
+    #     print("test")
         
 
 class Ui_Camera_API_Developer(Structure_Ui_Camera):
     logger_level = logging.INFO
 
     def __init__(self, *args, onj = None, logger_level = logging.INFO, **kwargs):
-        super(Ui_Camera_API_Developer, self).__init__(*args, **"kwargs")
+        super(Ui_Camera_API_Developer, self).__init__(*args, **kwargs)
 
         self.logger_level = logger_level
 
         self.init()
     
     def init(self):
-        self.configure_Other_Settings()
-
-    def configure_Button_Connections(self):
-        self.connect_camera_button.clicked.connect(
-            lambda: self.connect_to_Camera(
-                camera_flag=CAMERA_FLAGS.CV2
-                
-            )
-        )
-
+        self.configure_Other_Settings()  
+   
     def configure_Other_Settings(self):
         
         self.init_qt_graphicsView_Scene(
@@ -67,6 +63,18 @@ class Ui_Camera_API_Developer(Structure_Ui_Camera):
             self.frame_4,
         )
 
+    def configure_Button_Connections(self):
+        self.connect_camera_button.clicked.connect(
+            # lambda: self.connect_to_Camera(
+            #     CAMERA_FLAGS.CV2,
+            #     self.spinBox_Buffer_Size.value(),
+            #     self.exposure_Time
+            # )
+            self.test
+        )
+    def test(self):
+        print("test")
+    
     def closeEvent(self, *args, **kwargs):
         super(Ui_Camera_API_Developer, self).closeEvent(*args, **kwargs)
 
--- prototip1/run.py
+++ prototip1/run.py
@@ -1,11 +1,11 @@
 import libs
 from structure_ui import init_and_run_UI
-from main_window_constructor import Ui_Camera_API_Main
+from main_window_constructor import Ui_Camera_API_Developer
 
 
 if __name__ == "__main__":
     app, ui = init_and_run_UI(
         "Camera Developer UI",
-        Ui_Camera_API_Main,
+        Ui_Camera_API_Developer,
         UI_File_Path="main_window.ui"
     )
