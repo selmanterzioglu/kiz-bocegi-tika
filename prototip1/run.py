d7459e83e9d3eb5ab63cb5fade0fa2ea7d5b1d88
Muhammed Said BILGEHAN <msaidbilgehan@gmail.com>
2022-02-22 20:26:07 +0300

    cleanup

--- prototip1/run.py
+++ prototip1/run.py
@@ -1,13 +1,8 @@
 import libs
 from structure_ui import init_and_run_UI
-from structure_system import System_Object
 from main_window_constructor import kiz_UI
-# from main import kiz_UI
-
 
 if __name__ == "__main__":
-    # system_o = System_Object()
-    # system_o.thread_print_info()
     
     app, ui = init_and_run_UI(
         title="Kız Böcegi TIKA Kontrol Yazilimi",
