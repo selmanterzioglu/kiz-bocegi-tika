0c6176ee988e7912b4084f7fc55c77e6d0ed771e
Muhammed Said BILGEHAN <msaidbilgehan@gmail.com>
2022-02-22 19:15:58 +0300

    Fixed freeze of cameras, cleanup of code and ui, added ui v2 file

--- prototip1/main_window_constructor.py
+++ prototip1/main_window_constructor.py
@@ -2,69 +2,27 @@ import libs
 
 import logging
 from PyQt5.QtWidgets import *
-from PyQt5 import uic
-from PyQt5.QtGui import QImage, QPixmap
 from structure_ui import Structure_UI, Graphics_View
-from structure_ui_camera import Structure_Ui_Camera
 from structure_camera import Camera_Object, CAMERA_FLAGS
 from structure_system import System_Object
-import sys
 import cv2
 import qt_tools
 
 
-# from qt_tools import numpy_To_QT_Type_Converter
-
-class Ui_Camera_API_Main(Structure_UI):
-    
-    logger_level = logging.INFO
-
-    def __init__(self, *args, obj=None, logger_level= logging.INFO, **kwargs):
-        super(Ui_Camera_API_Main, self).__init__(*args, **kwargs)
-
-        self.logger_level = logger_level
-
-class Ui_Camera_API_Developer(Structure_Ui_Camera):
-    logger_level = logging.INFO
+class kiz_UI(Structure_UI):
 
     def __init__(self, *args, onj = None, logger_level = logging.INFO, **kwargs):
-        super(Ui_Camera_API_Developer, self).__init__(*args, **kwargs)
+        super(kiz_UI, self).__init__(*args, **kwargs)
+
+        self.cameras = dict()
+        self.q_timers = dict()
+        self.system_o = System_Object()
 
         self.logger_level = logger_level
-        self.camera_Instance = None
-        system_o = System_Object()
-        print(system_o.thread_print_info())
-        # system_o.thread_print_info()
-        self.cam_1 = Camera_Object(
-            camera_flag=CAMERA_FLAGS.CV2,
-            logger_level=logging.INFO,
-            auto_configure=False,
-            trigger_quit=None,
-            trigger_pause=None,
-            lock_until_done=False,
-            acquisition_framerate=30,
-            # exposure_time=exposure_time,
-            # max_buffer_limit=buffer_size,
-            max_buffer_limit=20
-            # logger_level=self.logger_level
-        )
-        self.cam_2 = Camera_Object(
-            camera_flag=CAMERA_FLAGS.CV2,
-            logger_level=logging.INFO,
-            auto_configure=False,
-            trigger_quit=None,
-            trigger_pause=None,
-            lock_until_done=False,
-            acquisition_framerate=30,
-            # exposure_time=exposure_time,
-            # max_buffer_limit=buffer_size,
-            max_buffer_limit=20
-            # logger_level=self.logger_level
-        )
+        self.system_o.thread_print_info()
         
-
-
-        self.q_timer_1 = qt_tools.qtimer_Create_And_Run(
+        self.is_Camera_Stream_Active = False
+        self.q_timers["camera_1_renderer"] = qt_tools.qtimer_Create_And_Run(
             self,
             connection=self.qtimer_function_1,
             delay=10,
@@ -72,7 +30,7 @@ class Ui_Camera_API_Developer(Structure_Ui_Camera):
             is_single_shot=False
         )
 
-        self.q_timer_2 = qt_tools.qtimer_Create_And_Run(
+        self.q_timers["camera_2_renderer"] = qt_tools.qtimer_Create_And_Run(
             self,
             connection=self.qtimer_function_2,
             delay=10,
@@ -85,47 +43,75 @@ class Ui_Camera_API_Developer(Structure_Ui_Camera):
     def init(self):
         self.configure_Other_Settings()
 
-
     def configure_Other_Settings(self):
         # self.label.setText(self.get_camera_available_port())
-        pass
+        pass 
+       
+    def camera_Initializes(self):
+        if not self.cameras.get("camera_1"):
+            self.cameras["camera_1"] = Camera_Object(
+                camera_flag=CAMERA_FLAGS.CV2,
+                logger_level=logging.INFO,
+                auto_configure=False,
+                trigger_quit=None,
+                trigger_pause=None,
+                lock_until_done=False,
+                acquisition_framerate=30,
+                # exposure_time=exposure_time,
+                # max_buffer_limit=buffer_size,
+                max_buffer_limit=10
+                # logger_level=self.logger_level
+            ) 
+        if not self.cameras.get("camera_2"):
+            self.cameras["camera_2"] = Camera_Object(
+                camera_flag=CAMERA_FLAGS.CV2,
+                logger_level=logging.INFO,
+                auto_configure=False,
+                trigger_quit=None,
+                trigger_pause=None,
+                lock_until_done=False,
+                acquisition_framerate=30,
+                # exposure_time=exposure_time,
+                # max_buffer_limit=buffer_size,
+                max_buffer_limit=10
+                # logger_level=self.logger_level
+            )
 
     def qtimer_function_1(self):
-        if self.cam_1 is not None:
-            self.frame_1.set_Background_Image(self.cam_1.stream_Returner(auto_pop=True, pass_broken=True))
+        if self.cameras.get("camera_1"):
+            self.frame_1.set_Background_Image(self.cameras["camera_1"].stream_Returner(auto_pop=True, pass_broken=True))
     
     def qtimer_function_2(self):
-        if self.cam_2 is not None:
-            self.frame_2.set_Background_Image(self.cam_2.stream_Returner(auto_pop=True, pass_broken=True))
+        if self.cameras.get("camera_2"):
+            self.frame_2.set_Background_Image(self.cameras["camera_2"].stream_Returner(auto_pop=True, pass_broken=True))
     
+    def camera_Remove(self):
+        if self.is_Camera_Instance_Exist():
+            self.stream_Switch(False)
+
+            self.cameras["camera_1"].quit()
+            self.cameras["camera_2"].quit()
+            
+            self.cameras.pop("camera_1")
+            self.cameras.pop("camera_2")
+        #return self.is_Stream_Active()
     
     def configure_Button_Connections(self):
         self.connect_camera_button.clicked.connect(            
             lambda: [
-                # self.connect_to_Camera(
-                #     CAMERA_FLAGS.CV2,
-                #     buffer_size=1000,
-                #     exposure_time=40000,
-                #     auto_configure = False
-                # ),
-                
-                # self.camera_Instance.api_CV2_Camera_Create_Instance(
-                #     # self.spinBox_Camera_Selector.value(), 
-                #     2,
-                #     extra_params=[]
-                # ),
+                self.camera_Initializes(),
 
-                self.cam_1.api_CV2_Camera_Create_Instance(1, extra_params = []),
-                self.cam_2.api_CV2_Camera_Create_Instance(2, extra_params = []),
+                self.cameras["camera_1"].api_CV2_Camera_Create_Instance(1, extra_params = []),
+                self.cameras["camera_2"].api_CV2_Camera_Create_Instance(2, extra_params = []),
                 
-                self.cam_1.stream_Start_Thread(
-                    number_of_snapshot=1000,
+                self.cameras["camera_1"].stream_Start_Thread(
+                    number_of_snapshot=-1,
                     delay = 0.001,
                     trigger_pause=self.is_Stream_Active,
                     trigger_quit=self.is_Quit_App
                 ),
-                self.cam_2.stream_Start_Thread(
-                        number_of_snapshot=1000,
+                self.cameras["camera_2"].stream_Start_Thread(
+                        number_of_snapshot=-1,
                         delay=0.001,
                         trigger_pause=self.is_Stream_Active,
                         trigger_quit=self.is_Quit_App
@@ -134,7 +120,6 @@ class Ui_Camera_API_Developer(Structure_Ui_Camera):
                 self.cam_1_status_label.setText(self.cam_1_status_label.text() + " ENABLED"),
                 self.cam_2_status_label.setText(self.cam_2_status_label.text() + " ENABLED"),
                 self.stream_Switch(True)
-                
             ]
         )
         self.remove_camera_button.clicked.connect(
@@ -142,12 +127,11 @@ class Ui_Camera_API_Developer(Structure_Ui_Camera):
                 self.camera_Remove,
                 self.cam_1_status_label.setText("Cam 1 Status:"),
                 self.cam_2_status_label.setText("Cam 2 Status:")
-
             ]
         )
 
     def closeEvent(self, *args, **kwargs):
-        super(Ui_Camera_API_Developer, self).closeEvent(*args, **kwargs)
+        super(kiz_UI, self).closeEvent(*args, **kwargs)
 
     def get_camera_available_port(self):
         source = 10
@@ -165,7 +149,11 @@ class Ui_Camera_API_Developer(Structure_Ui_Camera):
             temp_text += "," + i
         return temp_text
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = Ui_Camera_API_Main()
-    sys.exit(app.exec_())
+    def is_Stream_Active(self):
+        return self.is_Camera_Stream_Active
+
+    def stream_Switch(self, bool=None):
+        self.is_Camera_Stream_Active = \
+            not self.is_Camera_Stream_Active \
+            if bool is None else bool
+        return self.is_Camera_Stream_Active
--- prototip1/run.py
+++ prototip1/run.py
@@ -1,8 +1,8 @@
 import libs
 from structure_ui import init_and_run_UI
 from structure_system import System_Object
-from main_window_constructor import Ui_Camera_API_Developer
-# from main import Ui_Camera_API_Developer
+from main_window_constructor import kiz_UI
+# from main import kiz_UI
 
 
 if __name__ == "__main__":
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     # system_o.thread_print_info()
     
     app, ui = init_and_run_UI(
-        "Kız Böcegi TIKA Kontrol Yazilimi",
-        Ui_Camera_API_Developer,
-        UI_File_Path="main_window.ui"
+        title="Kız Böcegi TIKA Kontrol Yazilimi",
+        Class_UI=kiz_UI,
+        UI_File_Path="main_window_v2.ui"
     )
