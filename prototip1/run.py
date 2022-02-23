5831e231ba6039db5e56621b6fdcae2cb60eb957
selmantercioglu <selmantercioglu@gmail.com>
2022-02-23 03:15:20 +0300

    updated main window title

--- prototip1/run.py
+++ prototip1/run.py
@@ -5,7 +5,7 @@ from main_window_constructor import kiz_UI
 if __name__ == "__main__":
     
     app, ui = init_and_run_UI(
-        title="Odonata TIKA Control Software",
+        title="Kiz Bocegi TIKA Control Software",
         Class_UI=kiz_UI,
         UI_File_Path="main_window_v1.ui"
     )
