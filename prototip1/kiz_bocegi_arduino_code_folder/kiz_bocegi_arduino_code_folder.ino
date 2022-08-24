5979efb6d405977ebf4803a3542b6813292c659c
selmantercioglu <selmantercioglu@gmail.com>
2022-08-24 12:28:44 +0300

    fixed lock problem

--- prototip1/kiz_bocegi_arduino_code_folder/kiz_bocegi_arduino_code_folder.ino
+++ prototip1/kiz_bocegi_arduino_code_folder/kiz_bocegi_arduino_code_folder.ino
@@ -15,7 +15,7 @@
 //      LIBRARY      //
 ///////////////////////
 #include <AccelStepper.h>
-
+#include <TimerOne.h>
 ///////////////////////
 // DEVELOPER MODE    //
 ///////////////////////
@@ -31,31 +31,20 @@
 #define MotorInterfaceType 4 // for l298n : "4" 
 
 // Backend distance sensor
-#define TRIG_PIN_BACKEND 5
 #define ECHO_PIN_BACKEND 4  
+#define TRIG_PIN_BACKEND 5
 
 // Frontend distance sensor
-#define TRIG_PIN_FRONT 6 
-#define ECHO_PIN_FRONT 7
+#define ECHO_PIN_FRONT 6
+#define TRIG_PIN_FRONT 7 
 
 ////////////////////
 // ENUM VARIABLES //
 ////////////////////
-enum motors {
+enum motor_status {
   MOTORS_FORWARD,
   MOTORS_BACKWARD,
-  MOTORS_REVERSE,
-  MOTORS_OFF,
-  MOTORS_ON,
-  MOTORS_STOP,
-  MOTORS_RUN,
-  MOTORS_RESET
-};
-
-enum print_flags{
-  MOTORS,
-  DISTANCE_FRONT,
-  DISTANCE_BACKEND
+  MOTORS_STOP
 };
 
 enum sensors{
@@ -69,74 +58,76 @@ enum sensors{
 
 typedef struct kiz_bocegi_code_shared{
 
+  motor_status status;
+  
+  int route = 1;
+  int lock_motor = 0;
+
   long time_wave_front;   // front wave time value
   long distance_front;    // front distance value
 
   long time_wave_backend; // backend wave time value
   long distance_backend;  // backend distance value
+  
+  int motor_speed = 500;  // default value is 500 for motors_speed
 
-  // int a1, a2;
+  int stop_distance_backend_sensor; // if backend  distance less equal than this value, motors stops 
+  int stop_distance_frontend_sensor;// if frontend distance less equal than this value, motors stops
 
-  int stop_distance_backend_sensor;
-  int stop_distance_frontend_sensor;
+  volatile long ultrasonic_echo_start = 0;
 
   AccelStepper stepper_motors;
-
 
 } kiz_bocegi;
 
 kiz_bocegi g_shared;
 
-void distance_sensors_configurations(){
+void distance_sensors_configurations()
+{
   pinMode(TRIG_PIN_FRONT, OUTPUT);
   pinMode(TRIG_PIN_BACKEND, OUTPUT);
   pinMode(ECHO_PIN_FRONT, INPUT);
   pinMode(ECHO_PIN_BACKEND, INPUT);
+
+  set_sensor_stop_distance(9, 9);
 }
 
 void motor_configurations(){
-  g_shared.stepper_motors = AccelStepper(MotorInterfaceType, 4, 5, 6, 7);
+  g_shared.stepper_motors = AccelStepper(MotorInterfaceType, 8, 9, 10, 11);
+  g_shared.stepper_motors.setMaxSpeed(1000);
 }
 
-void setup(){
-
-  distance_sensors_configurations();
-  motor_configurations();
-
-  #if IS_PRINT
-    Serial.begin(9600);
-    delay(1000);
-  #endif
-  
+void set_speed(int speed)
+{
+  g_shared.motor_speed = speed;
 }
 
-void motors_status(motors flag){
-  // This function is update motors_status.  and then set motor_pins low or high value using  digitalWrite function.
+void set_motor_status(motor_status flag)
+{
+
+  // This function is update motors_status. 
+  // Then set motor_pins low or high value using  digitalWrite function.
 
   switch (flag)
   {
-  case MOTORS_FORWARD:
+    case MOTORS_FORWARD:
+        g_shared.stepper_motors.setSpeed(g_shared.motor_speed);
+        g_shared.stepper_motors.runSpeed();
+      break;
+    case MOTORS_BACKWARD:
+        g_shared.stepper_motors.setSpeed(-g_shared.motor_speed);
+        g_shared.stepper_motors.runSpeed();
+      break;
 
-    break;
-  case MOTORS_BACKWARD:
-
-    break;
-
-  case MOTORS_STOP:
-
-    break;
-
-  default:
-    #if IS_PRINT
-        Serial.print("Wrong Flag for motors_status(): " + String(flag) + "\n");
-    #endif
-    break;
+    case MOTORS_STOP:
+        g_shared.stepper_motors.stop();
+      break;
   }
 }
 
 int get_distance(sensors flag)
 {
-  // This function is get distance value from distance sensors
+  // This function gets distance value from distance sensors.
 
   int trig_pin, echo_pin;
   long *time_wave, *distance;
@@ -150,59 +141,26 @@ int get_distance(sensors flag)
       distance = &g_shared.distance_front;
       break;
     case DISTANCE_SENSORS_BACKEND:
-      trig_pin = TRIG_PIN_FRONT;
-      echo_pin = ECHO_PIN_FRONT;
+      trig_pin = TRIG_PIN_BACKEND;
+      echo_pin = ECHO_PIN_BACKEND;
       time_wave = &g_shared.time_wave_backend;
       distance = &g_shared.distance_backend;
       break;
   }
 
   digitalWrite(trig_pin, LOW); 
-  delayMicroseconds(0.01);
+  delayMicroseconds(10);
   digitalWrite(trig_pin, HIGH); 
-  delayMicroseconds(0.02);
+  delayMicroseconds(10);
   digitalWrite(trig_pin, LOW); 
+
   *time_wave = pulseIn(echo_pin, HIGH);
   *distance = *time_wave /29.1/2;
 
 }
 
-void print_info(print_flags flag){
-  // This function print all info variables to serial monitor. 
-  // You must choose which variable write to serial monitor using print_flags .
-  #if IS_PRINT
-
-    switch(flag){
-      case MOTORS:
-        Serial.print("INFO MOTORS:\n");
-        break;
-
-      case DISTANCE_FRONT:
-        Serial.println("Uzaklik On: " + String(g_shared.distance_front)); 
-        break;
-
-      case DISTANCE_BACKEND:
-        Serial.println("Uzaklik Arka: " + String(g_shared.distance_backend)); 
-        break;
-        
-    }
-    delay(PRINT_DELAY_TIME);
-
-  #endif
-
-}
-
-void print_info_all(){
-  //  if IS_PRINT is true, this function writes all variables to serial monitor.
-
-    #if IS_PRINT
-      print_info(MOTORS);
-      print_info(DISTANCE_FRONT);
-      print_info(DISTANCE_BACKEND);
-    #endif
-}
-
-void threasold_distance_sensors(sensors flag){
+void threasold_distance_sensors(sensors flag)
+{
   // if distance value is greater than 200, we set distance value to 200.
   
   switch (flag)
@@ -216,53 +174,156 @@ void threasold_distance_sensors(sensors flag){
     if(g_shared.distance_backend > 200)  
       g_shared.distance_backend = 200;
     break;
-  
-  default:
-    #if IS_PRINT
-        Serial.print("Wrong Flag for set_pin_distance(): " + String(flag) + "\n");
-    #endif
-    break;
   }
-
 }
 
-void test_code_go_without_sensors(){
-  
-  
-}
-
-void loop() {
-  
-  test_code_go_without_sensors();
-  
-  // scenario();
-}
-
-void set_sensor_stop_distance(int frontend_distance, int backend_distance){
+void set_sensor_stop_distance(int frontend_distance, int backend_distance)
+{
   g_shared.stop_distance_backend_sensor  = backend_distance;
   g_shared.stop_distance_frontend_sensor = frontend_distance;
 }
 
-int get_frontend_distance(){
-  return g_shared.distance_front;
-}
-int get_backend_distance(){
-  return g_shared.distance_backend;
+void check_distance_sensors(sensors sensor){
+  get_distance(sensor);
+  threasold_distance_sensors(sensor);
 }
 
-void scenario(){
+void read_serial(){
+  String serial_message = "";
+
+  if (Serial.available() > 0)
+  {
+      serial_message = Serial.readString();
+      serial_message.trim();
+  }
+
+  if (serial_message == "motors_forward")
+  {
+    set_motor_status(MOTORS_FORWARD);
+  }
+  else if (serial_message == "motors_backward")
+  {
+    set_motor_status(MOTORS_BACKWARD);
+  }
+  else if (serial_message == "motors_stop")
+  {
+    set_motor_status(MOTORS_STOP);
+  }
+  else if (serial_message == "motors_unlock"){
+    g_shared.lock_motor = 0;
+  }
+  else if (serial_message.indexOf("new_speed: ") != -1)
+  {
+      serial_message.remove(0, 11);
+      int new_speed = serial_message.toInt();
+      set_speed(new_speed);
+  }
+
+}
+
+void test_code_go_without_sensors()
+{
+  set_speed(500);
+  set_motor_status(MOTORS_FORWARD);
+}
+
+void interrupt ()
+{
+  /*
+    https://gelecegiyazanlar.turkcell.com.tr/konu/egitim/arduino-401/zaman-kesmesi-timer-interrupt 
+  */
+  cli();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+  OCR1A = 15624;
+  TCCR1B |= (1 << WGM12);
+  TCCR1B |= (1 << CS12) | (1 << CS10);
+  TIMSK1 |= (1 << OCIE1A);
+
+  sei();
+
+}
+
+
+void setup()
+{
+  distance_sensors_configurations();
+  motor_configurations();
+  // interrupt();
+
+  #if IS_PRINT
+    Serial.begin(9600);
+    delay(1000);
+  #endif
+}
+
+ISR(TIMER1_COMPA_vect)
+{  
+  Serial.print("[DEBUG] ");
+  Serial.print(" route: " + String(g_shared.route));
+  Serial.print(" lock: " + String(g_shared.lock_motor));
+  Serial.print(" front: " + String(g_shared.distance_front));
+  Serial.println(" backend: " + String(g_shared.distance_backend));
+
+  if(g_shared.route == 1)
+    check_distance_sensors(DISTANCE_SENSORS_FRONT);
+  
+  else if (g_shared.route == -1)
+    check_distance_sensors(DISTANCE_SENSORS_BACKEND);
+
+}
+
+void test_print_distance()
+{
+  check_distance_sensors(DISTANCE_SENSORS_FRONT);
+  check_distance_sensors(DISTANCE_SENSORS_BACKEND);
+
+  Serial.print("[DEBUG] ");
+  Serial.print(" front: " + String(g_shared.distance_front));
+  Serial.println(" backend: " + String(g_shared.distance_backend));
+
+}
+
+void scenario()
+{
   //  below lines are functions which calculate distance using distance_sensors and print info to serial monitor.
   
-  threasold_distance_sensors(DISTANCE_SENSORS_FRONT);
-  threasold_distance_sensors(DISTANCE_SENSORS_BACKEND);
 
-  if (Serial.find("forward")){
-    String data = Serial.readString();
-    motors_status(MOTORS_FORWARD);
-  }
-  else if (Serial.find("backward")){
-    String data = Serial.readString();
-    motors_status(MOTORS_BACKWARD);
-  }
+  read_serial();
 
+  if (g_shared.route == 1)
+  {
+    if (g_shared.distance_front <= g_shared.stop_distance_frontend_sensor)
+    {
+      set_motor_status(MOTORS_STOP);
+      g_shared.route = -1;
+      g_shared.lock_motor = 1;
+
+    }
+    if (g_shared.lock_motor == 0)
+      set_motor_status(MOTORS_FORWARD);
+    
+  }
+  else if (g_shared.route == -1) 
+  {
+    if (g_shared.distance_backend <= g_shared.stop_distance_backend_sensor)
+    {
+      set_motor_status(MOTORS_STOP);
+      g_shared.route = 1;
+      g_shared.lock_motor = 1;
+    }
+    if (g_shared.lock_motor == 0)
+      set_motor_status(MOTORS_BACKWARD);
+
+  }
 }
+
+void loop()
+{
+  // test_print_distance();
+  // test_code_go_without_sensors();
+  
+  scenario();
+}
+
